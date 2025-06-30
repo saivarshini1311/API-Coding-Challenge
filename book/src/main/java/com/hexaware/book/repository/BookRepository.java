f1c7776ebe67ba17930b299e361bac216a287107
Saivarshini R <saiv93585@gmail.com>
2025-06-30 10:24:14 +0530

    Delete book directory

--- book/src/main/java/com/hexaware/book/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hexaware.book.repository;
-
-import com.hexaware.book.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
-
